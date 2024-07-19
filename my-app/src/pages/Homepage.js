cb7aa0208229b517e4b4c6c8f5e659ea2514f838
AkotoB <aboadi010@st.ug.edu.gh>
2024-07-19 13:58:39 +0000

    Edit to 1/4 of homepage and addes other features

--- my-app/src/pages/Homepage.js
+++ my-app/src/pages/Homepage.js
@@ -1,8 +1,12 @@
-import image7 from './image7.png'
+import image7 from './image7.png';
 import chlogo from './chlogo2.png';
+import icon1 from './Icon1.png';
+import icon2 from './icon2.png';
+import icon3 from './icon3.png';
 
 const Homepage = () => {
     return(
+        <>
         
         <div className="imagecontainer" style={{backgroundImage:{image7}}}>
             <div className="welcome1">
@@ -25,6 +29,78 @@ const Homepage = () => {
             </div>
 
         </div>
+        <div className="relevance">
+            <br></br>
+            <br></br>
+            <div>
+                <p style ={{fontSize:"-50px",fontWeight:"lighter"}}>NIC</p>
+                <p style ={{fontSize:"30px",fontWeight:"bold"}}>A CHURCH THAT'S RELEVANT</p>
+            </div>
+            <div className="involved">
+                <div className="getinvolved1">
+                    <div className="iconbox" >
+                        <img src={icon1} className="icon" alt="icon of description"   ></img>
+
+                    </div>
+                    <div className='description'>
+                        <h3>ABOUT US</h3>
+                        <p>Welcome to New International Church,a vibrant community of faith in Accra,Ghana.We are dedicated to spreading God's love and serving our community.
+                            Join us on this journey of faith. hope and love.
+                        </p>
+                    </div>
+                    <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div style={{backgroundColor:"#FFD2A4", width:'411px',height:'16px'}} >
+
+                </div>
+
+                </div>
+                <div className="getinvolved">
+                <div className="iconbox" >
+                        <img src={icon2} className="icon" alt="icon of description"   ></img>
+
+                    </div>
+                    <div className='description'>
+                        <h3>GET INVOLVED</h3>
+                        <p>Join our community in serving others! Volunteer,participate in outreach programs, and engage in small groups to deepen your connection with God
+                            and our church family.
+                        </p>
+                    </div>
+                    <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div style={{backgroundColor:"#FFD2A4", width:'411px',height:'16px'}} >
+
+                </div>
+                </div>
+                <div className="getinvolved">
+                <div className="iconbox" >
+                        <img src={icon3} className="icon" alt="icon of description"   ></img>
+
+                </div>
+                <div className='description'>
+                        <h3>GIVING BACK</h3>
+                        <p>Sharing God's love through service and generosity. Support our outreach programs, missions, and community initiatives, making 
+                            a tangible difference in the lives of others.</p>
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div style={{backgroundColor:"#FFD2A4", width:'411px',height:'16px'}} >
+
+                </div>
+                    
+                </div>
+            </div>
+
+
+        </div>
+        </>
+        
     
         )
 }
